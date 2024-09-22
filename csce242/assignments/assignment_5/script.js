3b08b4868ae5af9b47788c875d4b633ce37c1f59
leemoorman <Leethalweapon487@gmail.com>
2024-09-22 14:06:15 -0400

    yer

--- csce242/assignments/assignment_5/script.js
+++ csce242/assignments/assignment_5/script.js
@@ -1,18 +1,18 @@
 function getRandomInt(max){
     return Math.floor(Math.random() * max);
-}
+};
 
 let index = 0;
 const countOnClick = document.getElementById("count");
 countOnClick.onclick = () =>{
     index += 1;
     document.getElementById("number").innerHTML = index;
-}
+};
 
 const randomImg = document.getElementById("rdm-image");
 randomImg.onclick = () =>{
     location.reload();
-}
+};
 
 let position = document.getElementById("myRange").value;
 const slider = document.getElementById("myRange");
