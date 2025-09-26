2d8eb905e4fd977f3bc200963aeb77d8ee21c566
leemoorman <Leethalweapon487@gmail.com>
2025-09-26 01:02:52 -0400

    fixed flexing issue

--- csce242/fall_25/assignments/07_loops/script.js
+++ csce242/fall_25/assignments/07_loops/script.js
@@ -1,9 +1,11 @@
-const sun = document.getElementById("sun");
-const moon = document.getElementById("moon");
-
 document.getElementById("scene").onclick = () =>{
+    //declaring variables
     const time = new Date();
     let hours = time.getHours();
+    const sun = document.getElementById("sun");
+    const moon = document.getElementById("moon");
+    let x_cloud = 370;
+    let x_tree = 415;
 
     //sets background based on time
     if(hours >= 18 || hours <= 6){
@@ -14,5 +16,31 @@ document.getElementById("scene").onclick = () =>{
         document.body.style.backgroundColor = "lightblue";
         moon.classList.add("hide");
         sun.classList.remove("hide");
+    }
+
+    //loop that creates trees and clouds
+    for(let i = 0; i < 6; i++){
+        let container = document.getElementById("main-content");
+        let contentDiv = document.createElement("div");
+        let cloudDiv = document.createElement("div");
+        let foliageDiv = document.createElement("div");
+        let trunkDiv = document.createElement("div");
+
+        cloudDiv.classList.add("cloud");
+        foliageDiv.classList.add("foliage");
+        trunkDiv.classList.add("trunk");
+
+        cloudDiv.style.top = "70px";
+        foliageDiv.style.top = "70px";
+        trunkDiv.style.top = "140px";
+
+        cloudDiv.style.left = `${x_cloud + (i * 25)}px`;
+        foliageDiv.style.left = `${x_tree + (i * 25)}px`;
+        trunkDiv.style.left = `${x_tree + (i * 25)}px`;
+
+        container.append(contentDiv);
+        contentDiv.append(cloudDiv);
+        contentDiv.append(trunkDiv);
+        contentDiv.append(foliageDiv);
     }
 }
